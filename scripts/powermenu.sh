0ae997aa3a326042a433c16e9e6b9640c369810e
Sudarshan <sudarshandevkota22@gmail.com>
2022-08-27 10:11:08 +0545

    logout icon fixed for powermenu

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-chosen=$(printf "  Power Off\n  Restart\n  Log Out" | rofi -dmenu -i -font "Hack 15" )
+chosen=$(printf "  Power Off\n  Restart\n  Log Out" | rofi -dmenu -i -font "Hack 15" )
 
 case "$chosen" in
 	"  Power Off") poweroff ;;
 	"  Restart") reboot ;;
-	"  Log Out") i3-msg exit ;;
+	" Log Out") i3-msg exit ;;
 	*) exit 1 ;;
 esac
