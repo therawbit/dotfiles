865e397f893c740be4bcdced6140dd86b5f9fcc7
Sudarshan <sudarshandevkota22@gmail.com>
2023-06-29 10:36:06 +0545

    multi screen option implemented

--- scripts/randr.sh
+++ scripts/randr.sh
@@ -1,15 +1,21 @@
-#! /bin/bash
+#!/bin/bash
 
-numberOfDevices=$(xrandr | grep connected -w | wc -l)
+chosen=$(printf "󱂫  Join Screen\n  Mirror Screen\n󰍹  Off" | rofi -dmenu -i -p "Multi Screen" -font "Hack 15" )
 secPolybarInstance=$(ps -ax | grep "\spolybar -r sec" | awk '{print $1}' | ps -ax | grep "\spolybar -r sec" | awk '{print $1}')
-if [ $numberOfDevices -eq 2 ]
+
+if [ "$chosen" = "󱂫  Join Screen" ]
 then
-	xrandr --output HDMI-1 --right-of eDP-1 --auto
+    xrandr --output HDMI-1 --right-of eDP-1 --auto
 	if [ "$secPolybarInstance" = "" ]
 	then	
 		polybar -r sec 2>/dev/null | tee -a /tmp/polybar1.log & disown
-	fi	
-else 
+	fi
+elif [ "$chosen" = "  Mirror Screen" ]
+then
 	kill $secPolybarInstance
+    xrandr --output HDMI-1 --same-as eDP-1 --auto
+elif [ "$chosen" = "󰍹  Off" ]
+then
+    kill $secPolybarInstance
 	xrandr --output HDMI-1 --off
-fi	
+fi    
