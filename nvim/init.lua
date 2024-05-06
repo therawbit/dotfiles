319bc9cb9f0e55259e34bdb415902981da02bf80
TheRawBit <sudarshandevkota22@gmail.com>
2024-05-06 14:15:54 +0545

    null-ls bug fixed

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,4 +12,4 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
-require("lazy").setup("plugins")
+
--- nvim/lua/plugins/nonels.lua
+++ nvim/lua/plugins/nonels.lua
@@ -4,6 +4,7 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+        require("none-ls.diagnostics.eslist_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.erb_lint,
