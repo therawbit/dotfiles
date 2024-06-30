17c4ab7b6e2b87ab2ff72fe3a36ee05d1543277b
therawbit <sudarshandevkota22@gmail.com>
2024-06-30 17:23:22 +0545

    nonels removed

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -15,23 +15,6 @@ return {
 
         local servers = require("mason-lspconfig").get_installed_servers()
 
-        local on_attach = function(client, bufnr)
-            vim.api.nvim_buf_set_option(bufnr, 'omnifunc',
-                'v:lua.vim.lsp.omnifunc')
-
-            local register_keymap = require("keybindings").register_keymap
-            register_keymap('n', 'gD', { vim.lsp.buf.declaration, buffer = bufnr, desc = "Go to declaration" })
-            register_keymap('n', 'gd', { vim.lsp.buf.definition, buffer = bufnr, desc = "Go to definition", })
-            register_keymap('n', 'K', { vim.lsp.buf.hover, buffer = bufnr, desc = "View docs" })
-            register_keymap('n', 'gi', { vim.lsp.buf.implementation, buffer = bufnr, desc = "Go to implementation" })
-            register_keymap('i', '<C-k>', { vim.lsp.buf.signature_help, buffer = bufnr, desc = "Signature help" })
-            register_keymap('n', '<space>D',
-                { vim.lsp.buf.type_definition, buffer = bufnr, desc = "Go to type definition" })
-            register_keymap('n', '<space>rn', { vim.lsp.buf.rename, buffer = bufnr, desc = "Rename symbol" })
-            register_keymap('n', '<space>ca', { vim.lsp.buf.code_action, buffer = bufnr, desc = "View code action" })
-            register_keymap('n', 'gr', { vim.lsp.buf.references, buffer = bufnr, desc = "Go to references" })
-            register_keymap('n', '<space>f', { vim.lsp.buf.format, buffer = bufnr, desc = "Format document" })
-        end
 
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities =
