942a56aa9d6d1196c6e8c4fb8c092e6794cb9c45
therawbit <sudarshandevkota22@gmail.com>
2024-07-18 18:00:40 +0545

    i3 and other

--- wofi/emoji/launch.sh
+++
@@ -1,6 +0,0 @@
-selected=$(cat ~/.config/wofi/emoji/list | wofi -d -i -p "Emoji")
-# if no options selected
-if test -z "$selected"; then
-    exit
-fi
-echo "$selected" | cut -d " " -f 1 -s | wl-copy -n
--- wofi/utilities/utilities.sh
+++
@@ -1,9 +0,0 @@
-
-#! /bin/bash
-chosen=$(printf "Bluetooth Device Battery\nNepali Date" | wofi -d  -i -p "Utilities" )
-
-case "$chosen" in
-	"Bluetooth Device Battery") ~/.config/scripts/bluetooth-battery.sh;;
-	"Nepali Date") ~/.config/scripts/nepalidate.sh;;
-	*) exit 1 ;;
-esac
