3f78a972d74a4ec70a788b3e53f822113f694c5f
therawbit <sudarshandevkota22@gmail.com>
2024-08-22 10:58:28 +0545

    added ocr

---
+++ scripts/ocr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Use slurp to select a region, grim to capture the screenshot, and save it to a temporary file
+screenshot=$(mktemp --suffix=.png)
+grim -g "$(slurp)" "$screenshot"
+
+# Run tesseract OCR on the screenshot and store the output in a variable
+text=$(tesseract "$screenshot" -)
+# Copy the recognized text to the clipboard using cliphist
+echo "$text" | wl-copy
+
+# Clean up the temporary screenshot file
+rm "$screenshot"
+
--- wofi/utilities/utilities.sh
+++ wofi/utilities/utilities.sh
@@ -1,9 +1,10 @@
 
 #! /bin/bash
-chosen=$(printf "Bluetooth Device Battery\nNepali Date" | wofi -d  -i -p "Utilities" )
+chosen=$(printf "Bluetooth Device Battery\nNepali Date\nOCR" | wofi -d  -i -p "Utilities" )
 
 case "$chosen" in
 	"Bluetooth Device Battery") ~/.config/scripts/bluetooth-battery.sh;;
 	"Nepali Date") ~/.config/scripts/nepalidate.sh;;
+  "OCR") ~/.config/scripts/ocr.sh;;
 	*) exit 1 ;;
 esac
